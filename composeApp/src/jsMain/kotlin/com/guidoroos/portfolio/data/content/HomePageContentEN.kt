0b998df2035d3cb513e25d96c27c0a945a254c15
guido <guidoroos@protonmail.com>
2026-05-02 13:07:14 +0200

    try fix issue

--- composeApp/src/jsMain/kotlin/com/guidoroos/portfolio/data/content/HomePageContentEN.kt
+++ composeApp/src/jsMain/kotlin/com/guidoroos/portfolio/data/content/HomePageContentEN.kt
@@ -9,7 +9,7 @@ val HomePageContentEN = HomeData(
     subtitle = "Roos Mobile",
     locationInfo = "Based in Eindhoven. Available for on-site collaboration as well in for example in Amsterdam, Rotterdam, Utrecht, Antwerp, and Düsseldorf",
     pitch = """
-    A successful app starts with the right technical strategy for your budget. Whether it's scaling a new idea through the efficiency of Kotlin Multiplatform, or delivering the high-end native UI that users expect on iOS and Android: I ensure the technology supports your business goals. By sharing logic where possible and building native where it matters, I offer a solution that sits right between 'fully separate teams' and 'limited cross-platform tools.' ⚖
+    A successful app starts with the right technical strategy for your budget. Whether it's scaling a new idea through the efficiency of Kotlin Multiplatform, or delivering the high-end native UI that users expect on iOS and Android: I ensure the technology supports your business goals. By sharing logic where possible and building native where it matters, I offer a solution that sits right between 'fully separate teams' and 'limited cross-platform tools.' 
 
     This approach is backed by $yearsExperience years of experience in mobile development, sharpened through numerous projects for a wide range of clients at a leading agency. From rescuing stalled projects to strategically reinforcing established development teams: I provide the seniority needed to make apps technically sound and future-proof.
 """.trimIndent(),
