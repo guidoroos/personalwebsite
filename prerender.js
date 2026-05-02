e6911e19df1bbc899f11877a605cce961ebcdeb5
guido <guidoroos@protonmail.com>
2026-05-02 12:54:43 +0200

    try fix pipeline

--- prerender.js
+++ prerender.js
@@ -26,7 +26,9 @@ const routes = [
 ];
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
 
   const languages = ['nl', 'en'];
